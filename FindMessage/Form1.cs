a41bed97c03dd0512dd7244347a1f67c3c79d123
MizukiUsami <50206898+MizukiUsami@users.noreply.github.com>
2020-03-03 14:34:06 +0800

    Add files via upload

---
+++ FindMessage/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace FindMessage
+{
+    public partial class Form1 : Form
+    {
+        public static Form1 form1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            form1 = this;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool right_num = true;
+            foreach (var item in textBox1.Text)
+            {
+                if (item > 57 || item < 48 && item != 42)
+                {
+                    right_num = false;
+                }
+            }
+
+            if (textBox1.Text.Length != 11 || !right_num)
+            {
+                MessageBox.Show("请输入正确电话号码");
+            }
+            else
+            {
+                richTextBox1.Clear();
+                progressBar1.Value = 0;
+                backgroundWorker1.RunWorkerAsync();              //调用backgroundWorker1_DoWork
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Focus();//获取焦点
+            richTextBox1.Select(richTextBox1.TextLength, 0);//光标定位到文本最后
+            richTextBox1.ScrollToCaret();//滚动到光标处
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //子线程中
+            MessageBox.Show("即将开始检索");
+            find f = new find();
+            f.Findstart(textBox1.Text, textBox2.Text);
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //主线程中
+            progressBar1.Value = e.ProgressPercentage;
+            label3.Text = progressBar1.Value.ToString() + "%";
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBar1.Value = 100;
+            label3.Text = "100%";
+            MessageBox.Show("检索完成");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("ilogs.txt");
+        }
+    }
+}
---
+++ FindMessage/find.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace FindMessage
+{
+    class find
+    {
+        int unknown_number;
+        List<int> record_unkown = new List<int>(); //获取*号位
+        List<string> list = new List<string>();
+
+        public void Findstart(string tel, string Ct)
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                if (tel[i] == '*')
+                {
+                    unknown_number += 1;
+                    record_unkown.Add(i);
+                }
+            }
+
+            double maxtimes = Math.Pow(10, unknown_number);
+
+            for (int j = 0; j < maxtimes; j++)
+            {
+                string fill_num = j.ToString().PadLeft(unknown_number, '0');
+                char[] combine_num = tel.ToCharArray();
+                for (int i = 0; i < record_unkown.ToArray().Length; i++)
+                {
+                    combine_num[record_unkown[i]] = fill_num[i];
+                }
+
+                string ss = HttpGet("http://mobsec-dianhua.baidu.com/dianhua_api/open/location?tel=" + new string(combine_num));
+
+                /**/
+                Regex re = new Regex("(?<=\").*?(?=\")", RegexOptions.None);
+                MatchCollection mc = re.Matches(ss);
+                foreach (Match ma in mc)
+                {
+                    list.Add(ma.Value);
+                }
+
+                //Console.WriteLine(list[2]);   //电话
+                //Console.WriteLine(list[10]);  //城市
+
+                /**/
+
+                if (list.Count != 37)
+                {
+                    continue;
+                }
+
+                string number = list[2];
+                string city = list[10];
+
+                if (Ct.Length != 0)
+                {
+                    if (city == Ct)
+                    {
+                        LogWrite(number + " " + city + Environment.NewLine);
+                    }
+                }
+                else
+                {
+                    LogWrite(number + " " + city + Environment.NewLine);
+                }
+
+                list.Clear();
+
+                Form1.form1.richTextBox1.AppendText(ss + Environment.NewLine);
+                Form1.form1.backgroundWorker1.ReportProgress((int)(j / maxtimes * 100));
+
+
+            }
+        }
+
+
+        public string HttpGet(string url)
+        {
+            Encoding encoding = Encoding.UTF8;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.Accept = "text/html, application/xhtml+xml, */*";
+            request.ContentType = "application/json";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+
+        public void LogWrite(string str)
+        {         //项目根目录
+            string path = "ilogs.txt";
+
+            if (!File.Exists(path))
+            {
+                FileStream fs = new FileStream(path, FileMode.Create, FileAccess.ReadWrite);
+
+                StreamWriter sw = new StreamWriter(fs);
+                sw.Write(str);
+                sw.Flush();
+                sw.Close();
+            }
+            else
+            {
+                FileStream fs = new FileStream(path, FileMode.Append);
+                //文本写入
+                StreamWriter sw = new StreamWriter(fs);
+                sw.Write(str);
+                sw.Flush();
+                sw.Close();
+            }
+        }
+    }
+}
